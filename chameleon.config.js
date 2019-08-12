023301e6e6382fc61632e28442b7a1c85c104b06
zhangshangjin <zhangshangjin@weidian.com>
2019-08-12 11:35:30 +0800

    《feat》《修改导出文件》

--- chameleon.config.js
+++ chameleon.config.js
@@ -1,21 +1,22 @@
-
 // 设置静态资源的线上路径
-const publicPath = '//www.static.chameleon.com/cml';
+const publicPath = "//www.static.chameleon.com/cml";
 // 设置api请求前缀
-const apiPrefix = 'https://api.chameleon.com';
+const apiPrefix = "https://api.chameleon.com";
 
 cml.config.merge({
   templateLang: "cml",
   templateType: "html",
-  platforms: ["web","weex","wx","alipay","baidu"],
+  platforms: ["web", "wx", "alipay"],
   buildInfo: {
-    wxAppId: '123456'
+    wxAppId: "123456"
   },
   wx: {
-    dev: {
-    },
+    dev: {},
     build: {
       apiPrefix
+    },
+    export: {
+      entry: ["src/components"]
     }
   },
   web: {
@@ -27,11 +28,13 @@ cml.config.merge({
       analysis: false,
       publicPath: `${publicPath}/web/`,
       apiPrefix
+    },
+    export: {
+      entry: ["src/components"]
     }
   },
   weex: {
-    dev: {
-    },
+    dev: {},
     build: {
       publicPath: `${publicPath}/weex/`,
       apiPrefix
@@ -41,5 +44,4 @@ cml.config.merge({
       apiPrefix
     }
   }
-})
-
+});
---
+++ dist/export/web/common/web_global.js
@@ -0,0 +1,7 @@
+
+import Vue from 'vue';
+require('./web_global.css');
+window.Vue = Vue;
+window.CML = {};
+window.CML.Vue = Vue;
+
